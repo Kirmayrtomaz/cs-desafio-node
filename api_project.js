620e6ae1d6373ab0fe4d471551286b63b19c4aea
Kirmayr Tomaz <tomaz.kirmayr@gmail.com>
2016-11-12 01:36:15 -0400

    Inserindo documentacao do projeto

---
+++ api_project.js
@@ -0,0 +1,13 @@
+define({
+  "name": "Construct Solutions Desafio Node",
+  "version": "0.0.1",
+  "description": "Construção da api para o desafio node",
+  "apidoc": "0.2.0",
+  "sampleUrl": false,
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-11-12T05:33:11.213Z",
+    "url": "http://apidocjs.com",
+    "version": "0.16.1"
+  }
+});
