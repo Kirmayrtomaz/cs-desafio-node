650ce7f8b6dda7b049a10def56ffb8c1772120e1
Kirmayr Tomaz <tomaz.kirmayr@gmail.com>
2016-11-12 00:52:05 -0400

    gh-pages update

---
+++ api_project.js
@@ -0,0 +1,13 @@
+define({
+  "name": "Concrete Solutions Desafio Node",
+  "version": "0.0.1",
+  "description": "Construção da api para o desafio node",
+  "apidoc": "0.2.0",
+  "sampleUrl": false,
+  "generator": {
+    "name": "apidoc",
+    "time": "2016-11-12T04:38:48.936Z",
+    "url": "http://apidocjs.com",
+    "version": "0.16.1"
+  }
+});
